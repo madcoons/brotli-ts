96f9516b8c4a56385dff54d12cc8fbad632201cd
Miroljub <miroljub1995@gmail.com>
2023-10-25 08:25:30 +0200

    Build realloc

--- build-wasm/compress.c
+++ build-wasm/compress.c
@@ -7,6 +7,14 @@ void init_buffer_result(size_t* buffer_size, uint8_t** encoded_buffer, size_t* d
   *data_length = 0;
 }
 
+void double_buffer_result(size_t* buffer_size, uint8_t** encoded_buffer, size_t* available_out, uint8_t** next_out) {
+  size_t offset = *next_out - *encoded_buffer;
+  *available_out += *buffer_size;
+  *buffer_size *= 2;
+  *encoded_buffer = (uint8_t*) realloc(*encoded_buffer, *buffer_size);
+  *next_out = *encoded_buffer + offset;
+}
+
 void free_buffer_result(uint32_t* result) {
   free((uint8_t*)result[1]);
   free(result);
@@ -39,9 +47,11 @@ uint32_t* compress_buffer(int quality, int lgwin, int mode, size_t input_size, c
   size_t available_out = *buffer_size;
   uint8_t* next_out = *encoded_buffer;
   for(;;) {
+    BROTLI_BOOL is_eof = available_in == 0;
+
     if(!BrotliEncoderCompressStream(
       s,
-      BROTLI_OPERATION_FINISH,
+      is_eof ? BROTLI_OPERATION_FINISH : BROTLI_OPERATION_PROCESS,
       &available_in,
       &next_in,
       &available_out,
@@ -54,6 +64,10 @@ uint32_t* compress_buffer(int quality, int lgwin, int mode, size_t input_size, c
 
     if (BrotliEncoderIsFinished(s)){
       return result;
+    }
+
+    if (available_out == 0) {
+      double_buffer_result(buffer_size, encoded_buffer, &available_out, &next_out);
     }
   }
 }
