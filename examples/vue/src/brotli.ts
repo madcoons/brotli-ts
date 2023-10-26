325b92914862e0ac0ebf55376f8a3b8df39d587f
Miroljub <miroljub1995@gmail.com>
2023-10-26 13:16:20 +0200

    Update examples

--- examples/vue/src/brotli.ts
+++ examples/vue/src/brotli.ts
@@ -4,15 +4,11 @@ import wasmDecoderUrl from "brotli-ts/wasm/decoder?url";
 import wasmEncoderUrl from "brotli-ts/wasm/encoder?url";
 
 export const compress = (buf: Uint8Array): Promise<Uint8Array> =>
-  ensureEncoderLoadedAsync(() =>
-    fetch(wasmEncoderUrl)
-      .then((x) => x.arrayBuffer())
-      .then((x) => new Uint8Array(x)),
-  ).then((encoder) => encoder.compressBuffer(buf));
+  ensureEncoderLoadedAsync(() => fetch(wasmEncoderUrl)).then((encoder) =>
+    encoder.compressBuffer(buf)
+  );
 
 export const decompress = (buf: Uint8Array): Promise<Uint8Array> =>
-  ensureDecoderLoadedAsync(() =>
-    fetch(wasmDecoderUrl)
-      .then((x) => x.arrayBuffer())
-      .then((x) => new Uint8Array(x)),
-  ).then((decoder) => decoder.decompressBuffer(buf));
+  ensureDecoderLoadedAsync(() => fetch(wasmDecoderUrl)).then((decoder) =>
+    decoder.decompressBuffer(buf)
+  );
